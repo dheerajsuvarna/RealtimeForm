a6410119394a5dd60a678135a350f4824019da03
Dheeraj <dheeraj.k050@gmail.com>
2018-05-28 18:31:33 +0200

    fixed truck scan api

--- public/js/api.js
+++ public/js/api.js
@@ -4,13 +4,18 @@ function readURL(input) {
     if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function (e) {
+        
       var image = e.target.result.split(',')[1];
       $.ajax({  
         type: "POST",  
         url: "/ticket/truck_scan_api",  
         data: {'image': image},  
-        success: function(dataString) {  
-           console.log("SUCCESS!!!")
+        
+        success: function(data) {  
+            alert('Vehicle Number Plate ' + data.results[0].plate +
+             '\n Vehicle Make ' + data.results[0].vehicle.make[0].name + 
+             '\n Vehicle Model ' + data.results[0].vehicle.make_model[0].name);  
+          
         }  
     });  
     }
--- server.js
+++ server.js
@@ -166,6 +166,7 @@ server.listen(process.env.PORT || 8080, function(){
 
 function truck_scan_api(request,response){
 var api = new OpenalprApi.DefaultApi()
+var dataRet;
 var imageBytes = request.body.image; // {String} The image file that you wish to analyze encoded in base64 
 var secretKey = "sk_af77a2481546119f83fce161"; // {String} The secret key used to authenticate your account.  You can view your  secret key by visiting  https://cloud.openalpr.com/ 
 var country = "eu"; // {String} Defines the training data used by OpenALPR.  \"us\" analyzes  North-American style plates.  \"eu\" analyzes European-style plates.  This field is required if using the \"plate\" task  You may use multiple datasets by using commas between the country  codes.  For example, 'au,auwide' would analyze using both the  Australian plate styles.  A full list of supported country codes  can be found here https://github.com/openalpr/openalpr/tree/master/runtime_data/config 
@@ -176,15 +177,15 @@ var opts = {
   'topn': 10, // {Integer} The number of results you would like to be returned for plate  candidates and vehicle classifications 
   'prewarp': "" // {String} Prewarp configuration is used to calibrate the analyses for the  angle of a particular camera.  More information is available here http://doc.openalpr.com/accuracy_improvements.html#calibration 
 };
-var callback = function(error, data, response) {
+var callback = function(error, data) {
   if (error) {
     console.error(error);
   } else {
     console.log('Vehicle Number Plate ' + JSON.stringify(data.results[0].plate));
     console.log('Vehicle Make ' + data.results[0].vehicle.make[0].name);
     console.log('Vehicle Model ' + data.results[0].vehicle.make_model[0].name);
+    return response.send(data);
   }
 };
 api.recognizeBytes(imageBytes, secretKey, country, opts, callback);
-
 }
